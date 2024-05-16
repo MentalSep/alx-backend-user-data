a794a2ab1cbcd6e13f7e96e016b611ae4798da14
MentalSep <chahidjava@hotmail.com>
2024-05-16 04:47:49 +0100

    new reg

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Filtering data from log message"""
-    pattern = '|'.join(f'({field}=[^;]*)' for field in fields)
-    redact = {'pattern': lambda match: re.sub(r'=.+', f'={redaction}',
-                                              match.group())}
-    return re.sub(pattern, redact['pattern'], message)
+    for field in fields:
+        message = re.sub(rf'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
