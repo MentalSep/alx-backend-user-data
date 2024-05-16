8557554fad4de98ef37550fc1873191f10ed9e57
MentalSep <chahidjava@hotmail.com>
2024-05-16 04:57:18 +0100

    new regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Filtering data from log message"""
-    for field in fields:
-        message = re.sub(rf'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+    pattern = rf'({"|".join(fields)})=([^{separator}]*)'
+    return re.sub(pattern,
+                  lambda match: f"{match.group(1)}={redaction}",
+                  message)
