5942b96513db09c051ccb71970f2b4e2dddf9d2e
MentalSep <chahidjava@hotmail.com>
2024-05-16 03:41:05 +0100

    add type annotaion

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module filttered_logger.py"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Filtering data from log message"""
     pattern = rf"(?:{separator})({'|'.join(fields)})=(.*?)(?={separator}|$)"
     return re.sub(pattern, rf"\1={redaction}", message)
