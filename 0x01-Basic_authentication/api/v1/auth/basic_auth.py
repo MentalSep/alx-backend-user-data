cb12522149831f49aeee4151b3dac953cda35f32
MentalSep <chahidjava@hotmail.com>
2024-05-21 05:45:17 +0100

    add task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,10 +63,12 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({'email': user_email})
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
         if users:
-            for user in users:
-                if user.is_valid_password(user_pwd):
-                    return user
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
 
         return None
