2cc00fc3399388687c845fb8994203f0a476dda8
MentalSep <chahidjava@hotmail.com>
2024-05-21 05:19:43 +0100

    add task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,14 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the base64 part of the Authorization header
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
