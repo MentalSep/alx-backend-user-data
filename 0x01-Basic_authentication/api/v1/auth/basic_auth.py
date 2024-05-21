002e5b84091a6f1e7f086a3a7f24445f0355346e
MentalSep <chahidjava@hotmail.com>
2024-05-21 05:56:59 +0100

    add task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,3 +72,14 @@ class BasicAuth(Auth):
                 return users[0]
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overloads Auth and retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        email, password = self.extract_user_credentials(decoded_header)
+
+        return self.user_object_from_credentials(email, password)
