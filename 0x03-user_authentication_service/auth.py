0a8eba6868dac9f8f785acbc7018d353134a86e2
MentalSep <chahidjava@hotmail.com>
2024-05-29 09:27:37 +0100

    add task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -39,3 +40,8 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Generate a UUID
+        """
+        return str(uuid.uuid4())
