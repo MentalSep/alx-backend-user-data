da53a91949226272c6ac15b697359de4db3f0871
MentalSep <chahidjava@hotmail.com>
2024-05-29 09:58:59 +0100

    add task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,10 +82,10 @@ class Auth:
         """
         Get a reset password token
         """
-        user = self._db.find_user_by(email=email)
-        if user is None:
-            raise ValueError()
-
-        token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=token)
-        return token
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
